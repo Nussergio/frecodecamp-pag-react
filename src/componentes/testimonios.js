619da120cc563d2972764182720072afa95886f0
facuhacosta <69871078+facuhacosta@users.noreply.github.com>
2022-03-29 17:24:21 -0300

    Update testimonios.js

--- src/componentes/testimonios.js
+++ src/componentes/testimonios.js
@@ -8,8 +8,8 @@ function Testimonio ({...props}){
       <img className='imagen-testimonio' src={ require (`../imagenes/testimonio-${imagen}.png`)}
       alt='foto de Shawn'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{nombre} Wang in {props.pais}</p>
-        <p className='cargo-testimonio'>{cargo} at {props.empresa}</p>
+        <p className='nombre-testimonio'>{nombre} Wang in {pais}</p>
+        <p className='cargo-testimonio'>{cargo} at {empresa}</p>
         <p className='texto-testimonio'>"{texto}"</p>
       </div>
     </div>
